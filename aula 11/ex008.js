43dc609819ec584db103374f06fb118a7e6bc335
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-01-11 02:02:29 -0300

    create ex008

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,8 +1,13 @@
-var vel = 12
+var vel = 12 // variavel velocidade // parte 1
 
-console.log(`A velocidade do seu carro é ${vel}Km/h`) // sempre que eu usar o ${} eu tenho que usar `` ao invés de ''
+console.log(`A velocidade do seu carro é ${vel}Km/h`) // sempre que eu usar o ${} eu tenho que usar `` ao invés de '' // vai ficar A velocidade do seu carro é 12Km/h // parte 2
 
-if (vel > 60) {
-        console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+if (vel > 60) { // Se a velocidade for maior do que 60 // parte 5
+
+        console.log('Você ultrapassou a velocidade permitida. MULTADO!') // esse só vai acontecer se a velocidade for maior que 60 // parte 3
 }
-console.log('Dirija sempre usando cinto de segurança!')
+console.log('Dirija sempre usando cinto de segurança!') // parte 4
+
+//pra escrever na tela usando o node.js nao usamos o document.white e sim o console.log
+
+// if - condição simples por que ela nao tem 'se não'
