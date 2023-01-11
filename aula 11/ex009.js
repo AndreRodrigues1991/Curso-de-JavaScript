5fbd6d4fa3137db6f4b9559ad6f442c3d3e6906c
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-01-11 02:02:44 -0300

    create ex009

---
+++ aula 11/ex009.js
@@ -0,0 +1,11 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`) // vivendo em EUA
+
+if (pais == 'brasil') { // Se o pais for igual Brasil
+    console.log('Você é Brasileiro!') // vc é Brasileiro
+} else { // Se não
+    console.log('Você é Estrangeiro!') // Você é Estrangeiro
+}
+
+// Se o pais for Brasil você é Brasileiro! se não você é Estrangeiro!
