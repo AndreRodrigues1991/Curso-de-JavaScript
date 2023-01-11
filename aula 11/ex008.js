3dc8f9f0ac0705e1b304aa9c96522b3c573d2d93
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-01-11 01:20:39 -0300

    create aula 11 ex008

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,1 +1,8 @@
-console.log('O console funcionou corretamente')
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`) // sempre que eu usar o ${} eu tenho que usar `` ao invés de ''
+
+if (vel > 60) {
+        console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
