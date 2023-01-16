1d5d0675c1a7fb35f99b70f010ef768ec4d2e3ef
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-01-16 03:59:18 -0300

    update

---
+++ aula 12/ex012.js
@@ -0,0 +1,9 @@
+var idade = 22
+if (idade < 16) {
+    console.log('não vota')
+} else {
+    if (idade < 18) { // se a idade for maior que 18...
+        console.log('Voto Opcional')
+
+    }
+} // Se a idade for menor que 16 nao vota se nao se a iadade for meno que 18 sabendo que menor que 18 maior igual a 16 ai o voto e opcional se nao se a iadade for maior igua a 18 ai o console.log vai ser  voto obrigatorio  rgwrg
