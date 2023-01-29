0d9a86c38dc3107188ca48f8f17bb8b41dbffe93
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-01-29 00:55:39 -0300

    create ex015

--- aula 12/ex015/script.js
+++ aula 12/ex015/script.js
@@ -1,1 +1,15 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear() // fullyear pq va pegar 4 digitos da data
+    var fano = document.getElementById('txtano') // ou essa
+    var res = document.querySelector('div#res') // ou essa
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verefique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value) // a idade vai ser o ano atual menos o valor digitado em fano
+        res.innerHTML = `Idade Calculada: ${idade}`
+    }
+}
+
+// A primeira coisa que tenho que fazer é pegar a data atual dpois o ano atual,
