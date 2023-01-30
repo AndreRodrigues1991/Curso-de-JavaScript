c108cb501c4a05addffce6a2a503cc9799063d2e
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-01-30 05:56:29 -0300

    create aula 13''

---
+++ aula 13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1 // var contador recebe 1
+while (c <= 6) { // enquanto o contador for menor ou igual que 6 ...
+//tudo o que eu escrever aqui dentro vai acontecer enquanto (c < 6) for verdade...
+    console.log('tudo bem?')
+    c++ // c = c + 1 ---- a gente viu isso durante as aulas de operadores...  ou seja o c valia 1 vai passar a valer 2 chegando no while vai voltar o c valendo 2 é menor do qq 6 ? sim entao ele vai voltar e vai somar mais 1 e vai vltando ate o c nao ser menor do que 6 ...
+}
+// Estrutura de repetição com teste lógico no início testa / executa e volta...   primeiro testa dpois executao bloco  no outro ..primeiro executa o bloco dpois ele testa
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
---
+++ aula 13/ambiente2.js
@@ -0,0 +1,6 @@
+var c = 1 // 
+do {  // faça
+    console.log(`passo ${c}`)
+    c++ 
+} while (c <= 6)
+//  fala isso tudo enquanto o C contador for menor ou igual a 6
