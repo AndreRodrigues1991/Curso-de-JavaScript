18ac32abea7cec173853db9922ef039344a57baa
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-02-02 03:53:32 -0300

    creatte aula 14

---
+++ aula 14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1 
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+// eu posso usar esse conceito abixo que é o mais usado do que o primeiro la de cima
+console.log('vai começar...')
+for(var c = 1;c <= 5;c++) {
+    console.log(c) //tudo que eu colocar aqui vai repetir 5 vezes 
+}
+console.log('FIM')
