861f4e74331c0e3760ac973236784346312cc133
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-02-11 02:35:39 -0300

    create update ambiente.js

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -20,3 +20,9 @@ console.log(`O primeiro valor do vetor é ${num[0]}`) // O primeiro valor do vet
 // num.sort()  ele coloca na ordem crescente os elementos ex: [5, 8, 2, 9, 3] vai ser [2, 3, 5, 8, 9]
 
 // for(let pos=0; pos<num.length; pos++) vai se inicializar no 0 no inicio enquanto ele nao chegar no final do meu vetor eu vou fazer pos++ pra ele adicionar 1 toda vez que fizer um looping
+
+// toda função precisa ter uma chamada
+
+// toda função pode ter os seguintes fatores uma chamada um conjunto de parâmetros uma ação e um retorno nem toda função tem parãmentros nem toda função tem retornos
+
+// ** funçoes são ações executadas assim que são chamadas ou em decorrência de algum evento.
