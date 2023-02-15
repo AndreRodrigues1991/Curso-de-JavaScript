b139f72eda3fdfffb600d9d36196641c56ebb792
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-02-15 02:35:32 -0300

    update

---
+++ desafio philipe/cript.js
@@ -0,0 +1,15 @@
+let cor = document.querySelector('input#cores')
+cor.addEventListener('click', clicarbotao)
+
+function clicarbotao() {
+    mudar.style.backgroundColor = 'yellow'
+   
+} 
+
+/*
+function  t(){
+    let cores = ["red" , "orange" , "blue"]
+    let numeroRandomico = Math.ceil(Math.random() * cores.length -1|)
+    return {numeroRandomico}
+} 
+*/
