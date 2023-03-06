a94cbb8ca4c5b83f7fdccf5740bb0b6eb4319597
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-03-06 03:00:57 -0300

    del e create

---
+++ Ex-Github-Professor/ex.github10/Acao.js
@@ -0,0 +1,17 @@
+let resp = document.getElementById('Saida')
+
+function Acao1() {
+    resp.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+
+function Acao2() {
+    resp.innerHTML += '<p> Clicou no segundo botão </p>'
+}
+
+function Acao3() {
+    resp.innerHTML += '<p> Clicou no terceiro botão </p>'
+}
+
+function Acao4() {
+    resp.innerHTML += '<p> Clicou no quarto botão </p>'
+}
