abb619b4e11eb73bf35381df2126762fe51b36ff
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-04-10 01:24:40 -0300

    update e finish
    o resultado é maior ou menor

--- Ex-Github-Professor/ex.github13/maior.js
+++ Ex-Github-Professor/ex.github13/maior.js
@@ -5,10 +5,10 @@ function maiornumero() {
 
     let res = document.querySelector('section#saida')
     if (n1 > n2) {
-        res.innerHTML = `<p>Analizando os valores ${n1} e ${n2}, O maior valor é <strong>${n1}</strong></p>`
+        res.innerHTML = `<p>Analizando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, O maior valor é <strong>${n1}</strong></p>`
     } else if (n1 < n2) {
-        res.innerHTML = `<p>Analizando os valores ${n1} e ${n2}, O maior valor é o <strong>${n2}</strong></p>`
+        res.innerHTML = `<p>Analizando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, O maior valor é o <strong>${n2}</strong></p>`
     } else {
-        res.innerHTML = `<p>Analizando os valores ${n1} e ${n2},  `
+        res.innerHTML = `<p>Analizando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, ambos são <strong>IGUAIS</strong></p>  `
     }
 }
