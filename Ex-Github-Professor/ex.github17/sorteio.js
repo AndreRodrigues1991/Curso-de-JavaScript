fc1bce773d1ea553bf7b0c745df6d4b59f6c35ce
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-04-19 02:48:05 -0300

    update ex.17
    Gerenciador de números
     res.innerHTML += `<p>Acabei de pensar no número <mark>${num}</mark></p>`
    
    function LimparNumero() {

--- Ex-Github-Professor/ex.github17/sorteio.js
+++ Ex-Github-Professor/ex.github17/sorteio.js
@@ -1,9 +1,15 @@
-function gerarnumero() {
+function GerarNumero() {
     let min = 1
     let max = 100
     let dif = max - min
-    let aleatorio = Math.random()
-    let num = min + Math.trunc(dif * aleatorio)
+    let aleatorio = Math.random() // Essa função gera um valor Real aleatório entre 0 e 1
+    let num = min + Math.trunc(dif * aleatorio) // a função Math.trunc () corta o ponto e os dígitos à direita dele.
 
-    let res = document.querySelector('section#result')
+    let res = document.querySelector('section#saida')
+    res.innerHTML += `<p>Acabei de pensar no número <mark>${num}</mark></p>`
+}
+
+function LimparNumero() {
+    let res = document.querySelector('section#saida')
+    res.innerHTML = null // Vai esvaziar a lixeira toda
 }
