07545e17e8a187bf82f43a5024e8f7c0bfd87fa8
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-04-19 02:01:51 -0300

    update

---
+++ Ex-Github-Professor/ex.github17/sorteio.js
@@ -0,0 +1,9 @@
+function gerarnumero() {
+    let min = 1
+    let max = 100
+    let dif = max - min
+    let aleatorio = Math.random()
+    let num = min + Math.trunc(dif * aleatorio)
+
+    let res = document.querySelector('section#result')
+}
