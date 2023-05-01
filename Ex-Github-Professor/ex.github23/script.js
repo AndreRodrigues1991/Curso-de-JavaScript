2b4117548fa0be8805f66fbc97ea8d7a668daf26
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-05-01 00:00:43 -0300

    update

--- Ex-Github-Professor/ex.github23/script.js
+++ Ex-Github-Professor/ex.github23/script.js
@@ -1,3 +1,12 @@
 function ContarNumero() {
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += '<h2>Numeros Pares de 1 até 10</h2>'
+    let cont = 2
+    while (cont <= 10) {
+        saida.innerHTML += `${cont} &#x1F449;`
+        cont += 2 // Corresponde a cont = cont + 2
+    }
+    saida.innerHTML += `&#x1F3C1;`
     
 }
