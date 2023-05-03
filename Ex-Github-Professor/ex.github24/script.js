c5154c005932715ff87aa7efbfa34faf88e9df3c
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-05-03 04:24:24 -0300

    create ex.24

---
+++ Ex-Github-Professor/ex.github24/script.js
@@ -0,0 +1,11 @@
+function ContarNumero() {
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += '<h2>Contagem regressiva de 10 a 1<h2>'
+    let cont = 10
+    while (cont >= 1) {
+        saida.innerHTML += `${cont} &#x1F449;`
+        cont -- // Corresponde a cont = cont - 1
+    }
+    saida.innerHTML += `&#x1F3C1;`
+}
