feda7a6cbae94959dac4cb77d067170851fec87e
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-05-28 02:53:06 -0300

    create desafio 06
    Conversor de temperatura
    
    @@ -0,0 +1,12 @@
    let btn = document.getElementById('btn')
    let res = document.getElementById('res')
    
    //evento
    btn.addEventListener('click', converter, false)
    
    //função
    function converter() {
        let temp = Number(prompt('Digite uma temperatura em C (Celcius):'))
        let
    
    }

---
+++ desafios/desafio 06/script.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById('btn')
+let res = document.getElementById('res')
+
+//evento
+btn.addEventListener('click', converter, false)
+
+//função
+function converter() {
+    let temp = Number(prompt('Digite uma temperatura em C (Celcius):'))
+    let   
+
+}
