35b2bc9045202ea49185376fe5e7fdca5a556208
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-05-30 05:06:06 -0300

    fim do desafio 06

--- desafios/desafio 06/script.js
+++ desafios/desafio 06/script.js
@@ -7,6 +7,12 @@ btn.addEventListener('click', converter, false)
 //função
 function converter() {
     let temp = Number(prompt('Digite uma temperatura em C (Celcius):'))
-    let   
+    
+    let k = temp + 273 // Não funciona com Decimal
+    let f = temp + 33
+    
+    res. innerHTML = `<h2>A temperatura ${temp}C, corresponde a ...</h2>
+    <p>${k}k (Kelvin).</p>
+    <p>${f}f (Fahrenheit).</p>`
 
 }
