d397b9e2c39839deaf1709b28ab85d0c08e41de2
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-06-06 01:06:36 -0300

    desafio 10 github guanabara

--- desafios/desafio 10/script.js
+++ desafios/desafio 10/script.js
@@ -7,5 +7,13 @@ function calculo() {
     let a = Number(prompt('Qual o valor de a?'))
     let b = Number(prompt('Qual o valor de b?'))
     let c = Number(prompt('Qual o valor de c?'))
-    let 
+    let total = Number(b*b) - 4* Number(a) * Number(c)
+
+    resultado.innerHTML = `
+    <h2>Fórmula de Bhaskara</h2>
+    <p>A equação atual é ${a}x² + ${b}x + ${c} = 0</p>
+    <p>O cálculo realizado será △ = ${b*b} - 4 . ${a} . ${c}</p>
+    <p>O valor calculado para delta foi △ = ${total}</p>
+    
+    `
 }
