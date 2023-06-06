3e0273252d8ab5ea44ee146daec31a40ba4d8624
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-06-06 00:56:48 -0300

    update

---
+++ desafios/desafio 10/script.js
@@ -0,0 +1,11 @@
+let bhk = document.getElementById('bhk')
+let resultado = document.getElementById('resultado')
+
+bhk.addEventListener('click', calculo, false)
+
+function calculo() {
+    let a = Number(prompt('Qual o valor de a?'))
+    let b = Number(prompt('Qual o valor de b?'))
+    let c = Number(prompt('Qual o valor de c?'))
+    let 
+}
